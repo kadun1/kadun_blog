f525f3f8e193aa0bc3a09a670368eb974a08ef01
kadun1 <kadunjj@gmail.com>
2022-06-03 20:49:30 +0900

    Initial commit

---
+++ src/main/java/com/kadun/kadun_blog/KadunBlogApplication.java
@@ -0,0 +1,13 @@
+package com.kadun.kadun_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KadunBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KadunBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kadun/kadun_blog/KadunBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kadun.kadun_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KadunBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
